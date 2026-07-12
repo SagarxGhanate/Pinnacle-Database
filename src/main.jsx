6b62b0a3632956f98fca2d1f6d8696c99462eb5e
Your Name <your.email@example.com>
2026-07-12 20:59:46 +0530

    fix: hardcode fallbacks for VITE_GOOGLE_CLIENT_ID and VITE_API_URL

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || '163523287398-c07l6o9974ek4dl0m686jjk8hb3mcols.apps.googleusercontent.com'}>
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>,
